0ca9e7e0d10b0f52c7b6b44eb25f0af21a456e43
Syrus Dark <naveen@syrusdark.website>
2020-07-06 13:04:24 +0530

    Dont use venv

--- .travis/runPycairox86.sh
+++ .travis/runPycairox86.sh
@@ -1,19 +1,13 @@
-
 cp cairocomplied/lib/x86/cairo.dll cairo/cairo.dll
 curl -L https://aka.ms/nugetclidl -o nuget.exe
 ./nuget install pythonx86 -Version $PYVER -OutputDirectory pythonx86
-./pythonx86/pythonx86.$PYVER/tools/python.exe -m venv buildcairo
-source buildcairo/Scripts/activate
-python -m pip install --upgrade pip
-python -m pip install --upgrade wheel
-python -m pip install --upgrade setuptools
-python -m pip install pytest
-python -m pip install --upgrade mypy || true
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install --upgrade pip
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install --upgrade wheel
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install --upgrade setuptools
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install pytest
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install --upgrade mypy || true
 export INCLUDE="$PWD/cairocomplied/include/"
 export LIB="$PWD/cairocomplied/lib/x86/"
 cmd.exe //c "RefreshEnv.cmd"
-python setup.py bdist_wheel
-cp buildcairo/dist/WHEELSNAMEx32 dist/WHEELSNAMEx32
-python -m pip install dist/WHEELSNAMEx32
-python testcairo.py
-
+./pythonx86/pythonx86.$PYVER/tools/python.exe setup.py bdist_wheel
+./pythonx86/pythonx86.$PYVER/tools/python.exe -m pip install dist/WHEELSNAMEx32
