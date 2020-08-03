fad7a32c82edb076204e60c6bc3ca46caecba8dc
Naveen M K <naveen@syrusdark.website>
2020-08-03 19:10:33 +0530

    Try meson build

--- .travis/downloadCairo.sh
+++ .travis/downloadCairo.sh
@@ -1,4 +1,11 @@
-
+pip install meson
+pip install ninja
+curl http://www.cairographics.org/releases/cairo-1.8.8.tar.gz -o cairo.tgz
 curl -L https://github.com/preshing/cairo-windows/releases/download/$CAIRO_VERSION/cairo-windows-$CAIRO_VERSION.zip -o cairocomplied.zip
 7z x cairocomplied.zip
+7z x cairo.tgz
+cd cairo
+meson compile
+cmd //c tree
+cd ../
 mv cairo-windows-$CAIRO_VERSION cairocomplied
