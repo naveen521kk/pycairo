79e7fa109c150483e21bc6271a655f38480be9ba
Syrus Dark <naveen@syrusdark.website>
2020-08-03 19:25:07 +0530

    Try building

--- .travis/runPycairox64.sh
+++ .travis/runPycairox64.sh
@@ -4,6 +4,14 @@ cp cairocomplied/lib/x64/cairo.dll cairo/cairo.dll
 choco install python --version=$PYVER
 export PATH="/c/$PYDIR:/c/$PYDIR/Scripts:$PATH"
 cmd.exe //c "RefreshEnv.cmd"
+pip install meson
+pip install ninja
+curl http://www.cairographics.org/releases/cairo-1.8.8.tar.gz -o cairo.tgz
+tar -xzf cairo.tgz
+cd cairo
+meson compile
+cmd //c tree
+cd ../
 python -m pip install --upgrade pip
 python -m pip install --upgrade wheel
 python -m pip install --upgrade setuptools
