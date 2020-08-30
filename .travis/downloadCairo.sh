e11e4d5008c5fff27a840e43a91fd9df3cbac4e2
Naveen M K <naveen@syrusdark.website>
2020-08-30 09:25:04 +0530

    Try tee?

--- .travis/downloadCairo.sh
+++ .travis/downloadCairo.sh
@@ -1,4 +1,4 @@
-
-curl -L https://github.com/preshing/cairo-windows/releases/download/$CAIRO_VERSION/cairo-windows-$CAIRO_VERSION.zip -o cairocomplied.zip
+#https://github.com/preshing/cairo-windows/releases/download/with-tee/cairo-windows-1.17.2.zip
+curl -L https://github.com/preshing/cairo-windows/releases/download/with-tee/cairo-windows-$CAIRO_VERSION.zip -o cairocomplied.zip
 7z x cairocomplied.zip
 mv cairo-windows-$CAIRO_VERSION cairocomplied
